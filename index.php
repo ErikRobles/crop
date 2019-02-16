a7e41abcf58bd040f8782a665407ab282e879a48
ErikRobles <erikjames69@hotmail.com>
2019-02-15 20:17:15 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<html lang="en">
+<head>
+  <title>PHP - jquery ajax crop image before upload using croppie plugins</title>
+  <script src="js/jquery.js"></script>
+
+ 
+  <script src="js/croppie.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href=" css/croppie.css">
+</head>
+<body>
+
+
+<div class="container">
+	<div class="panel panel-default">
+	  <div class="panel-heading">Image Upload</div>
+	  <div class="panel-body">
+
+
+	  	<div class="row">
+	  		<div class="col-md-4 text-center">
+				<div id="upload-demo" style="width:350px"></div>
+	  		</div>
+	  		<div class="col-md-4" style="padding-top:30px;">
+				<strong>Select Image:</strong>
+				<br/>
+				<input type="file" id="upload">
+				<br/>
+				<button class="btn btn-success upload-result">Upload Image</button>
+	  		</div>
+	  		<div class="col-md-4" style="">
+				<div id="upload-demo-i" style="background:#e1e1e1;width:300px;padding:30px;height:300px;margin-top:30px"></div>
+	  		</div>
+	  	</div>
+
+
+	  </div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+$uploadCrop = $('#upload-demo').croppie({
+    enableExif: true,
+    viewport: {
+        width: 200,
+        height: 200,
+        type: 'circle'
+    },
+    boundary: {
+        width: 300,
+        height: 300
+    }
+});
+
+
+$('#upload').on('change', function () { 
+	var reader = new FileReader();
+    reader.onload = function (e) {
+    	$uploadCrop.croppie('bind', {
+    		url: e.target.result
+    	}).then(function(){
+    		console.log('jQuery bind complete');
+    	});
+    	
+    }
+    reader.readAsDataURL(this.files[0]);
+});
+
+
+$('.upload-result').on('click', function (ev) {
+	$uploadCrop.croppie('result', {
+		type: 'canvas',
+		size: 'viewport'
+	}).then(function (resp) {
+
+
+		$.ajax({
+			url: "ajaxpro.php",
+			type: "POST",
+			data: {"image":resp},
+			success: function (data) {
+				html = '<img src="' + resp + '" />';
+				$("#upload-demo-i").html(html);
+			}
+		});
+	});
+});
+
+
+</script>
+<?php 
+
+$cropped_image = $_POST['image'];
+list($type, $cropped_image) = explode(';', $cropped_image);
+list(, $cropped_image) = explode(',', $cropped_image);
+$cropped_image = base64_decode($cropped_image);
+$image_name = date('ymdgis').'.png';
+file_put_contents('Uploads/'.$image_name, $cropped_image);
+// add insert/update query to save filename in the database table
+
+?>
+
+</body>
+</html>
